e8022fe69582330aff3d6314e120170dc4fbcf1c
malachigamblin <malachi.gamblin@gmail.com>
2023-04-28 20:45:12 -0400

    setup models

---
+++ models/Playlist.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Playlist extends Model {}
+
+Playlist.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    playlist_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "playlist",
+  }
+);
+
+module.exports = Playlist;
