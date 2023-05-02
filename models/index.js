452a476857214d22aee3c1538e378d967f145dee
malachigamblin <malachi.gamblin@gmail.com>
2023-05-02 19:53:46 -0400

    added comment.js

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require("./User");
 const Bio = require("./bio");
+const Comment = require("./comment");
 
 // Define associations between models
 User.hasOne(Bio, {
@@ -7,8 +8,24 @@ User.hasOne(Bio, {
   onDelete: "CASCADE",
 });
 
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
+
 Bio.belongsTo(User, {
   foreignKey: "user_id",
 });
 
-module.exports = { User, Bio };
+Bio.hasMany(Comment, {
+  foreignKey: "bio_id",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(Bio, {
+  foreignKey: "bio_id",
+});
+
+module.exports = { User, Bio, Comment };
