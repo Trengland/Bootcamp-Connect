5004db1ffe08e9a7362b2946cdd0ea1a0722b220
MOX UFO <mox.ufo@gmail.com>
2023-05-08 10:43:50 -0400

    updated feed-details

--- controllers/feed.js
+++ controllers/feed.js
@@ -18,17 +18,17 @@ router.get("/", async (req, res)=> {
       ],
     });
 
-    console.log(bioData)
+    // console.log(bioData)
     // bioData[0].user.name
     // Serialize data so the template can read it
-    // const peerbios = bioData.map((bio) => bio.get({ plain: true }));
+    const peerBios = bioData.map((bio) => bio.get({ plain: true }));
     // console.log("+++++++++++++++++++++++++++++++++++++++++++")
-    // console.log(peerbios)
-    // // Pass serialized data and session flag into template
-    // res.render('homepage', { 
-    //   p, 
-    //   logged_in: req.session.logged_in 
-    // });
+    console.log(peerBios[0])
+    // Pass serialized data and session flag into template
+    res.render('feed', { 
+      peerBios, 
+      logged_in: req.session.logged_in 
+    });
   } catch (err) {
     console.log(err)
     res.status(500).json(err);
