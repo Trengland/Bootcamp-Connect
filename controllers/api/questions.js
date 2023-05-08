6b4838e5eaea73dd38d519b42d692b07244e33f3
MOX UFO <mox.ufo@gmail.com>
2023-05-07 20:07:05 -0400

    the sing up process now lead to the feed handlebars

--- controllers/api/questions.js
+++ controllers/api/questions.js
@@ -5,20 +5,40 @@ const{ Bio }= require('../../models');
 
 
 router.post('/', async(req,res) =>{
+  console.log('ready to post');
   try {
-    const bioData = await Bio.create(
-    { 
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
-    });
-    console.log(bioData);
+    // const bioData = await Bio.create(
+    // { 
+    //   favorite_movies_or_tv_shows:"",
+    //   favorite_songs:"",
+    //   birth_day:"",
+    //   favorite_hobby:"",
+    //   favorite_quote:"",
+    //   github:"",
+    //   user_id: ""
+    // });
+    // console.log(bioData);
 
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;     
-    });
-    res.redirect('/feed') 
+    // req.session.save(() => {
+    //   req.session.user_id = userData.id;
+    //   req.session.logged_in = true;     
+    // });
+    res.redirect('/feed')
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
+router.get('/', async(req,res) =>{
+  // console.log('ready to redner questions');
+  try {
+
+    // req.session.save(() => {
+    //   req.session.user_id = userData.id;
+    //   req.session.logged_in = true;     
+    // });
+    res.render('questions')
   } catch (err) {
     console.log(err);
     res.status(400).json(err);
